9f0da4da010ed43410bf349461f160304db8705e
Robert Edmonds <edmondsfastly@users.noreply.github.com>
2021-09-15 17:53:16 -0400

    src/dnstap.proto → dnstap.pb/dnstap.proto

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 // Copyright 2021 Fastly, Inc.
 
 fn main() -> std::io::Result<()> {
-    prost_build::compile_protos(&["src/dnstap.proto"], &["src/"])?;
+    prost_build::compile_protos(&["dnstap.pb/dnstap.proto"], &["dnstap.pb/"])?;
     Ok(())
 }
