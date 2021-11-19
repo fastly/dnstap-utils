d4813ab8ddc577eb2d1cf96a9bf0cf5b1903aa0d
Robert Edmonds <edmondsfastly@users.noreply.github.com>
2021-11-19 16:20:37 -0500

    DnstapHandlerError: Add deserialization function
    This commit adds an ad hoc function for deserializing
    DnstapHandlerError's.
    
    Since the serialized DnstapHandlerError's that get written out by
    dnstap-replay need to eventually be deserialized and formatted for
    display, a function is needed to reverse the serialization performed by
    the DnstapHandlerError::serialize() method.
    
    Note that the serialization/deserialization pair of functions here
    doesn't quite perform a lossless 100% roundtrip. The additional String's
    attached to DnstapHandlerError::Mismatch are redundant and only used for
    logging purposes, so they don't get serialized out, and thus they can't
    be deserialized. But the Mismatch's String's aren't needed anyway.

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,5 @@
 use anyhow::{bail, Result};
-use bytes::{BufMut, Bytes, BytesMut};
+use bytes::{Buf, BufMut, Bytes, BytesMut};
 use std::convert::TryFrom;
 use std::net::IpAddr;
 use thiserror::Error;
@@ -18,7 +18,7 @@ pub fn try_from_u8_slice_for_ipaddr(value: &[u8]) -> Result<IpAddr> {
     }
 }
 
-#[derive(Error, Debug)]
+#[derive(Debug, Error, PartialEq)]
 pub enum DnstapHandlerError {
     #[error("Mismatch between logged dnstap response and re-queried DNS response, expecting {1} but received {2}")]
     Mismatch(Bytes, String, String),
@@ -30,30 +30,100 @@ pub enum DnstapHandlerError {
     MissingField,
 }
 
+const DNSTAP_HANDLER_ERROR_PREFIX: &[u8] = b"dnstap-replay/DnstapHandlerError\x00";
+
 impl DnstapHandlerError {
     pub fn serialize(&self) -> Bytes {
-        let prefix = b"dnstap-replay/DnstapHandlerError\x00";
         match self {
             DnstapHandlerError::Mismatch(mismatch, _, _) => {
-                let mut b = BytesMut::with_capacity(prefix.len() + 4 + mismatch.len());
-                b.extend_from_slice(prefix);
+                let mut b =
+                    BytesMut::with_capacity(DNSTAP_HANDLER_ERROR_PREFIX.len() + 4 + mismatch.len());
+                b.extend_from_slice(DNSTAP_HANDLER_ERROR_PREFIX);
                 b.put_u32(1);
                 b.extend_from_slice(mismatch);
                 b
             }
             DnstapHandlerError::Timeout => {
-                let mut b = BytesMut::with_capacity(prefix.len() + 4);
-                b.extend_from_slice(prefix);
+                let mut b = BytesMut::with_capacity(DNSTAP_HANDLER_ERROR_PREFIX.len() + 4);
+                b.extend_from_slice(DNSTAP_HANDLER_ERROR_PREFIX);
                 b.put_u32(2);
                 b
             }
             DnstapHandlerError::MissingField => {
-                let mut b = BytesMut::with_capacity(prefix.len() + 4);
-                b.extend_from_slice(prefix);
+                let mut b = BytesMut::with_capacity(DNSTAP_HANDLER_ERROR_PREFIX.len() + 4);
+                b.extend_from_slice(DNSTAP_HANDLER_ERROR_PREFIX);
                 b.put_u32(3);
                 b
             }
         }
         .freeze()
     }
+}
+
+pub fn deserialize_dnstap_handler_error(input: &[u8]) -> Result<DnstapHandlerError> {
+    if input.len() < DNSTAP_HANDLER_ERROR_PREFIX.len() {
+        bail!("Input buffer is too small");
+    }
+
+    let mut buf = Bytes::copy_from_slice(input);
+
+    let prefix = buf.copy_to_bytes(DNSTAP_HANDLER_ERROR_PREFIX.len());
+    if prefix != DNSTAP_HANDLER_ERROR_PREFIX {
+        bail!("DnstapHandlerError prefix not present");
+    }
+
+    if buf.remaining() < 4 {
+        bail!("DnstapHandlerError type not present");
+    }
+
+    match buf.get_u32() {
+        1 => Ok(DnstapHandlerError::Mismatch(
+            buf.split_off(0),
+            String::from(""),
+            String::from(""),
+        )),
+        2 => Ok(DnstapHandlerError::Timeout),
+        3 => Ok(DnstapHandlerError::MissingField),
+        _ => {
+            bail!("Unknown DnstapHandlerError type");
+        }
+    }
+}
+
+#[test]
+fn test_deserialize_dnstap_handler_error() {
+    // Test the extreme boundary condition. This is a minimally sized serialized
+    // DnstapHandlerError::Mismatch.
+    assert_eq!(
+        deserialize_dnstap_handler_error(b"dnstap-replay/DnstapHandlerError\x00\x00\x00\x00\x01")
+            .unwrap(),
+        DnstapHandlerError::Mismatch(Bytes::from(&b""[..]), String::from(""), String::from(""))
+    );
+
+    // This is the smallest serialized DnstapHandlerError::Mismatch with a single byte payload.
+    assert_eq!(
+        deserialize_dnstap_handler_error(
+            b"dnstap-replay/DnstapHandlerError\x00\x00\x00\x00\x01\x42"
+        )
+        .unwrap(),
+        DnstapHandlerError::Mismatch(
+            Bytes::from(&b"\x42"[..]),
+            String::from(""),
+            String::from("")
+        )
+    );
+
+    // The only way to serialize a DnstapHandlerError::Timeout.
+    assert_eq!(
+        deserialize_dnstap_handler_error(b"dnstap-replay/DnstapHandlerError\x00\x00\x00\x00\x02")
+            .unwrap(),
+        DnstapHandlerError::Timeout
+    );
+
+    // The only way to serialize a DnstapHandlerError::MissingField.
+    assert_eq!(
+        deserialize_dnstap_handler_error(b"dnstap-replay/DnstapHandlerError\x00\x00\x00\x00\x03")
+            .unwrap(),
+        DnstapHandlerError::MissingField
+    );
 }
